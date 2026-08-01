cec68086b8126d2617dc15680aa543cdcb671bc6
Anvitha Yerramsetty <anvithayerramsety@gmail.com>
2026-08-01 07:41:54 +0530

    Initial Playwright Framwork

--- tests/UI/ClientApp.spec.ts
+++ tests/UI/ClientApp.spec.ts
@@ -4,8 +4,8 @@ import { loginpayload } from '../../test_data/Loginpayload';
 
 
 test("Login flow",async({loginpage,page})=>{
-    test.setTimeout(60000)
-    const url="https://rahulshettyacademy.com/client/#/dashboard/dash"
+   // test.setTimeout(60000)
+    //const url="https://rahulshettyacademy.com/client/#/dashboard/dash"
    
     await loginpage.Goto()
     await page.waitForTimeout(5000)
